0274d1aa0a13c1d552b7885ad9e907b6a9642c95
Ivan Todorov Vasilev <IVAN TODOROV.VASILEV@local.ittcesena.it>
2022-03-11 09:16:19 +0100

    creazione classe Scuola

---
+++ esScuola/alunno.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace esScuola
+{
+    public class alunno
+    {
+        public int Property
+        {
+            get => default;
+            set
+            {
+            }
+        }
+
+        public void toString()
+        {
+            throw new System.NotImplementedException();
+        }
+
+        public void classBiennio()
+        {
+            throw new System.NotImplementedException();
+        }
+
+        public void classTriennio()
+        {
+            throw new System.NotImplementedException();
+        }
+    }
+}
---
+++ esScuola/scuola.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace esScuola
+{
+    public class scuola : alunno
+    {
+        private int field;
+        private int field1;
+        private int field2;
+        private int field3;
+    }
+}
